d2c11ec62169ab517386752eb812b28dd79e2801
Андрій Грицюк <venganpro12@gmail.com>
2024-04-05 11:52:58 +0300

    Some changes.

---
+++ src/app/add-choise/add-choise.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-choise',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-choise.component.html',
+  styleUrl: './add-choise.component.scss'
+})
+export class AddChoiseComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddChoiseComponent } from './add-choise/add-choise.component';
+import { CardsComponent } from './cards/cards.component';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
+import { AddCardComponent } from './cards/add-card/add-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "add", component: AddChoiseComponent },
+    { path: "add-card", component: AddCardComponent },
+    { path: "cards", component: CardsComponent },
+    { path: "login", component: LoginComponent },
+    { path: "register", component: RegisterComponent }
+];
